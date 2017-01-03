a2ae1a694eb919bd5ea7023cccc0b3abad111e86
vmaimone <vmaimone@users.noreply.github.com>
2017-01-03 09:38:34 -0500

    Update routes-table.js

--- template/server/plugins/routes-table.js
+++ template/server/plugins/routes-table.js
@@ -4,36 +4,55 @@ module.exports = function printRoutesTable(server) {
   const table = server.table()[0].table
   const uri = server.info.uri
   const entries = extractRoutes(table, entries => entries.map(stringifyRoute)).join('\n').trim()
-  console.log( ''
-    + seperator
-    + `method  path ${' '.repeat(47-uri.length)} Server running at: ${uri}`
-    + seperator
-    + entries
-    + seperator
+
+  server.route({
+    method: 'get',
+    path: '/meta/routes',
+    handler(request, reply) {
+      return reply(extractRoutes(table).reduce((dict, current) => {
+        let ns = current.ns || '/'
+        delete current.ns
+        if (!dict[ns]) dict[ns] = []
+        dict[ns].push(current)
+        return dict
+      }, {}))
+    }
+  })
+
+  console.log(
+    seperator +
+    `method  path ${' '.repeat(47 - uri.length)} Server running at: ${uri}` +
+    seperator +
+    entries +
+    seperator
   )
 }
 
-function stringifyRoute({method, path}) {
+function stringifyRoute({ method, path }) {
   let pad = ' '.repeat('method'.length - method.length)
-  if(path === '/') path = '/index.html'
+  if (path === '/') path = '/index.html'
   return `${method}  ${pad}${path}`
 }
 
 function extractRoutes(table, fn) {
   let entries = table.map(entry => {
-      let ns = entry.path.slice(1).split('/')[0].trim()
-      let path = entry.path
+    let ns = entry.path.slice(1).split('/')[0].trim().toLowerCase()
+    let path = entry.path
     let method = entry.method.toUpperCase()
     return { ns, path, method }
   })
 
   entries.sort((a, b) => {
-    return a.ns+a.method+a.path.replace(a.ns,'') > b.ns+a.method+b.path.replace(b.ns,'')
+    let aa = (a.ns + a.path.replace(a.ns, '') + a.method).toLowerCase()
+    let bb = (b.ns + b.path.replace(b.ns, '') + b.method).toLowerCase()
+    return aa === bb
+      ? 0
+      : (aa > bb ? 1 : -1)
   })
 
   if (typeof fn === 'function') {
     return fn(entries)
   } else {
-      return entries
+    return entries
   }
 }
