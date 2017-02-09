b3a27018640f3570be0d3338495dddf29c8a7277
vmaimone <vmaimone@users.noreply.github.com>
2017-02-09 06:03:55 -0500

    Update main.js

--- template/client/main.js
+++ template/client/main.js
@@ -1,11 +1,13 @@
-/* eslint-disable*/
-
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+Vue.use(VueRouter)
 
 window.App = new Vue({
   el: '#app',
   components: {
-    App
+    App,
+    router: new VueRouter(router)
   }
 })
