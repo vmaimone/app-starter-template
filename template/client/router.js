27cb8015fad07592c041ac5814fe267249e99ff2
vmaimone <vmaimone@users.noreply.github.com>
2017-02-09 06:27:17 -0500

    Update router.js

--- template/client/router.js
+++ template/client/router.js
@@ -1,4 +1,9 @@
-export default ({
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
   mode: 'hash',
   base: __dirname,
   routes: [
