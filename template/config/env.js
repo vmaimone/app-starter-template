206eb8671b94cbef148993b8c72b7313247b9413
vmaimone <vmaimone@users.noreply.github.com>
2017-08-04 17:56:50 -0400

    Update env.js
    cleanup sample env.js

--- template/config/env.js
+++ template/config/env.js
@@ -1,3 +1,3 @@
-process.env.PORT = (process.env.NODE_ENV === 'production') 
-  ? null // production (change me) 
+process.env.PORT = process.env.NODE_ENV === 'production'
+  ? null // production (change me)
   : 3000 // development
