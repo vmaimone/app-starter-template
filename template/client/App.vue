011160566051e646df88fe0e0691746d78ab1691
vmaimone <vmaimone@users.noreply.github.com>
2017-08-12 12:18:49 -0400

    add keys to $root.appuser

--- template/client/App.vue
+++ template/client/App.vue
@@ -17,7 +17,10 @@ export default {
   name: 'application',
   data() {
     return {
-      appuser: {}
+      appuser: {
+        username: 'guest',
+        isLoggedIn: false
+      }
     }
   }
 }
