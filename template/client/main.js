31f851c421882bca3089b2622e6a0c0f113a2aa3
vmaimone <vmaimone@users.noreply.github.com>
2017-09-26 05:42:32 -0400

    reconfigure how the main vue app is initialized

--- template/client/main.js
+++ template/client/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import Filters from './filters'
 import router from './router'
 import App from './App.vue'
 
-const el = '#app'
-const application = Object.assign(App, { el, router })
+Vue.use(Filters)
 
-window.App = new Vue(application)
+const el = '#app'
+const components = { App }
+
+window.App = new Vue({
+  el,
+  router,
+  components,
+  render(h) { return h(App) }
+})
